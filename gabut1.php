205ad8ece0198fc4a683378ce92943c843254cfc
Alief Dwi Arjuna  Alief Dwi Arjuna <aliefarjuna1892@gmail.com>
2018-11-20 07:11:58 +0700

    first

---
+++ gabut1.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Semoga Beruntung</title>
+</head>
+<body>
+	<div> 
+		<p align="center">Semoga Beruntung</p>
+		<p align="center">Silahkan dicari yg valid jika tidak ada silahkan refresh page</p>
+	</div>
+	<div align="center">
+		<?php
+	function create_random()
+	{
+    	$data = 'ABCDEFGHIJKLMNOPQRSTU1234567890';
+    	$string = '';
+    	for($i = 0; $i < 9; $i++) {
+        	$pos = rand(0, strlen($data)-1);
+        	$string .= $data{$pos};
+    	}
+    	return $string;
+	}
+
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/16'.create_random()."62<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	echo 'http://e.gift.id/u/83'.create_random()."46<br>";
+	?>
+	</div>
+		<p align="center">Jika error refresh page bisa contact langsung</p>
+</body>
+</html>
+
